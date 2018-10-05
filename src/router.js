4cb4701e45c89ed7f2f3f8afaf183b7d4ad271a8
uni <unibtc@gmail.com>
2018-10-06 05:11:35 +0800

    clean request dump format

--- src/router.js
+++ src/router.js
@@ -1,9 +1,17 @@
 const Router = require('koa-router');
-
 const config = require('./config');
-
 const router = new Router();
 
+function parseJsonToStr(obj) {
+  let str = '';
+  if (!obj) {
+    return `${str}\n`;
+  }
+  for (const [k, v] of Object.entries(obj)) {
+    str += `${k}: ${v}\n`;
+  }
+  return str;
+}
 router.get('/', async ctx => {
   await ctx.render('main', { apiurl: config.URL });
 });
@@ -11,10 +19,19 @@ router.all('/api/:user', ctx => {
   ctx.validateParam('user').isString().trim();
   const socketid = ctx.vals.user;
   const socket = ctx.app.io.socket;
-  let b = JSON.stringify(ctx.request, null, 2);
+  const b = `
+--General--
+Request URL: ${config.URL + ctx.request.url}
+Request Method: ${ctx.request.method}
 
-  b = b + JSON.stringify(ctx.request.body, null, 2);
+--Headers--
+${parseJsonToStr(ctx.request.headers)}
+--Query Parameters--
+${parseJsonToStr(ctx.request.query)}
+--Body Parameters--
+${parseJsonToStr(ctx.request.body)}
 
+`;
   socket.to(socketid).emit('result', b);
   ctx.body = 'ok';
 });
